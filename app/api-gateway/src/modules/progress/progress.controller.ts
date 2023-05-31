66ecfd996884f88713918fa6843326c510d119f3
Rocío <rplatini@fi.uba.ar>
2023-05-31 18:27:46 -0300

    Create complete-workout endpoint

--- app/api-gateway/src/modules/progress/progress.controller.ts
+++ app/api-gateway/src/modules/progress/progress.controller.ts
@@ -1,5 +1,6 @@
 import { HttpService } from '@nestjs/axios';
-import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { ServerController } from '../../shared/server-controller';
 import { AuthGuard } from '../auth/auth.guard';
 
@@ -8,5 +9,13 @@ import { AuthGuard } from '../auth/auth.guard';
 export class ProgressController extends ServerController {
   constructor(httpService: HttpService) {
     super(httpService, 'progress');
+  }
+
+  @Post('complete-workout')
+  async completeWorkout(@Body() completeWorkoutDto: any) {
+    const { data } = await firstValueFrom(
+      this.httpService.post('/progress/complete-workout', completeWorkoutDto)
+    );
+    return data;
   }
 }
