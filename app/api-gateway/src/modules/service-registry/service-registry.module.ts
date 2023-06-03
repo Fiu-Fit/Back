7eeef3b01dee4efc760f463819a034e089660610
ianshih <ianshih2003@gmail.com>
2023-06-03 17:26:04 -0300

    Fix

--- app/api-gateway/src/modules/service-registry/service-registry.module.ts
+++ app/api-gateway/src/modules/service-registry/service-registry.module.ts
@@ -1,22 +1,14 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServiceConfig, ServiceName } from '../../shared/service-config';
 import { ServiceRegistryController } from './service-registry.controller';
 import { ServiceRegistryService } from './service-registry.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    HttpModule.registerAsync({
-      imports:    [ServiceRegistryModule, HttpModule],
-      useFactory: (serviceRegistryService: ServiceRegistryService) => {
-        return ServiceConfig.createHttpModuleOptionsFromService(
-          serviceRegistryService,
-          ServiceName.User
-        );
-      },
-      inject: [ServiceRegistryService]
+    HttpModule.register({
+      baseURL: process.env.SERVICE_REGISTRY_URL
     })
   ],
   controllers: [ServiceRegistryController],
