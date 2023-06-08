2f7c3deb1bc4257cf5f8a74545b85db21f16675c
Ezequiel Schejtman <ZequiceteYT@gmail.com>
2023-06-08 20:05:08 -0300

    Fix Service URL & API Keys

--- app/api-gateway/src/shared/service-config.ts
+++ app/api-gateway/src/shared/service-config.ts
@@ -15,7 +15,7 @@ export const ServiceApiKeys: Record<string, string> = {
   workout:   'WORKOUT_API_KEY',
   user:      'USER_API_KEY',
   progress:  'PROGRESS_API_KEY',
-  goals:     'GOALS_API_KEY',
+  goals:     'PROGRESS_API_KEY',
   ratings:   'RATINGS_API_KEY',
   followers: 'USER_API_KEY'
 };
@@ -24,7 +24,7 @@ export const ServiceUrl: Record<string, string> = {
   workout:   'WORKOUT_SERVICE_URL',
   user:      'USER_SERVICE_URL',
   progress:  'PROGRESS_SERVICE_URL',
-  goals:     'GOALS_SERVICE_URL',
+  goals:     'PROGRESS_SERVICE_URL',
   ratings:   'RATINGS_SERVICE_URL',
   followers: 'USER_SERVICE_URL'
 };
