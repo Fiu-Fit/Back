a5ba13b2f4589450608d56f80b3fa055fefe67c1
CalmEsSimple <it@calmessimple.com.ar>
2023-06-24 18:12:22 -0300

    Add Verification table to shcema

---
+++ app/users-service/prisma/migrations/20230624211017_trainer_verification_table/migration.sql
@@ -0,0 +1,27 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[verificationId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateEnum
+CREATE TYPE "RequestStatus" AS ENUM ('Pending', 'Declined', 'Approved');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "verificationId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "Verification" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "requestStatus" "RequestStatus" NOT NULL,
+    "received_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Verification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_verificationId_key" ON "User"("verificationId");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_verificationId_fkey" FOREIGN KEY ("verificationId") REFERENCES "Verification"("id") ON DELETE SET NULL ON UPDATE CASCADE;
