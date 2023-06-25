3c7de2fd73df863b4eb5d528c0d6dc918c629b86
Rocío <rplatini@fi.uba.ar>
2023-06-25 11:45:16 -0300

    Modify message notification schema

---
+++ app/users-service/prisma/migrations/20230625031047_change_message_notifications/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `message_id` on the `MessageNotification` table. All the data in the column will be lost.
+  - Added the required column `sender_id` to the `MessageNotification` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "MessageNotification_message_id_key";
+
+-- AlterTable
+ALTER TABLE "MessageNotification" DROP COLUMN "message_id",
+ADD COLUMN     "sender_id" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "profile_url" TEXT;
