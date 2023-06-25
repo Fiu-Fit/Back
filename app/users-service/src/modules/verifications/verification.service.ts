06b0ff3349bba0f6cf406e90544031c59f19b193
Ian Shih <ianshih2003@gmail.com>
2023-06-24 23:09:24 -0300

    Feature/trainer verification (#18)
    * Add Verification table to shcema
    
    * Add resourceId to Verification table
    
    * Update migrations
    
    * Fix user typo in migration
    
    * Add Verification Module
    
    * Add Verification Service
    
    * Add endpoints
    
    ---------
    
    Co-authored-by: CalmEsSimple <it@calmessimple.com.ar>

---
+++ app/users-service/src/modules/verifications/verification.service.ts
@@ -0,0 +1,52 @@
+import { Page } from '@fiu-fit/common';
+import { Injectable } from '@nestjs/common';
+import { RequestStatus, Verification } from '@prisma/client';
+import { PrismaService } from '../../prisma.service';
+
+@Injectable()
+export class VerificationService {
+  constructor(private prismaService: PrismaService) {}
+
+  addVerificationRequest(
+    userId: number,
+    resourceId: string
+  ): Promise<Verification> {
+    return this.prismaService.verification.create({
+      data: {
+        userId,
+        resourceId,
+        status: RequestStatus.Pending
+      }
+    });
+  }
+
+  updateVerification(
+    id: number,
+    verification: Verification
+  ): Promise<Verification> {
+    return this.prismaService.verification.update({
+      where: { id },
+      data:  verification
+    });
+  }
+
+  async getVerificationRequests(
+    status?: RequestStatus
+  ): Promise<Page<Verification>> {
+    const verificationRequests = await this.prismaService.verification.findMany(
+      {
+        where: { status }
+      }
+    );
+    return {
+      rows:  verificationRequests,
+      count: verificationRequests.length
+    };
+  }
+
+  getVerification(id: number): Promise<Verification | null> {
+    return this.prismaService.verification.findUnique({
+      where: { id }
+    });
+  }
+}
