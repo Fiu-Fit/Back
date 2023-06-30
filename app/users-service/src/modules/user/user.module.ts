55b303fccb5e5300f47d5e56732871a67f4aa1b2
ianshih <ianshih2003@gmail.com>
2023-06-30 15:58:38 -0300

    WIP

--- app/users-service/src/modules/user/user.module.ts
+++ app/users-service/src/modules/user/user.module.ts
@@ -1,14 +1,9 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { PrismaService } from '../../prisma.service';
-import {
-  UserLocation,
-  UserLocationsSchema
-} from '../user-location/schema/user-location.schema';
+
 import { UserLocationModule } from '../user-location/user-location.module';
-import { UserLocationService } from '../user-location/user-location.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
@@ -18,12 +13,9 @@ import { UserService } from './user.service';
       envFilePath: ['.env']
     }),
     HttpModule,
-    UserLocationModule,
-    MongooseModule.forFeature([
-      { name: UserLocation.name, schema: UserLocationsSchema }
-    ])
+    UserLocationModule
   ],
   controllers: [UserController],
-  providers:   [UserService, PrismaService, UserLocationService]
+  providers:   [UserService, PrismaService]
 })
 export class UserModule {}
