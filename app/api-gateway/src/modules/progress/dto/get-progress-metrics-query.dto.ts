2ae8dd9e02a03ed9842fd035d02284e828d9fb05
Manuel Sanchez <manuelsfdelavega@gmail.com>
2023-06-30 14:16:12 -0300

    Add dto to progress controller

---
+++ app/api-gateway/src/modules/progress/dto/get-progress-metrics-query.dto.ts
@@ -0,0 +1,24 @@
+import { Type } from 'class-transformer';
+import { IsDateString, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class GetProgressMetricsQueryDTO {
+  @IsDateString()
+  @IsOptional()
+  start?: string;
+
+  @IsDateString()
+  @IsOptional()
+  end?: string;
+
+  @IsString()
+  @IsOptional()
+  exerciseId?: string;
+
+  @IsNumber()
+  @Type(() => Number)
+  @IsOptional()
+  userId?: number;
+
+  @IsOptional()
+  category?: number;
+}
